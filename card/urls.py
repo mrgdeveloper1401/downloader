1c1c3854a2d262778be8d91b8e1b9efb4c0b55d1
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-09-26 21:21:19 +0330

    delete api view

--- card/urls.py
+++ card/urls.py
@@ -7,5 +7,6 @@ from . import views
 app_name = 'card'
 urlpatterns = [
     path('lc_card/', views.LinkSciolListCreateApiView.as_view()),
+    path('d_card/<pk>/', views.LinkSciolDelete.as_view()),
     
 ]
--- card/views.py
+++ card/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
 from .serializers import LinkSciolSerializers
 from .models import LinkSciol
 
@@ -6,3 +6,8 @@ from .models import LinkSciol
 class LinkSciolListCreateApiView(ListCreateAPIView):
     serializer_class = LinkSciolSerializers
     queryset = LinkSciol.objects.all()
+    
+
+class LinkSciolDelete(DestroyAPIView):
+    queryset = LinkSciol.objects.all()
+    serializer_class = LinkSciolSerializers
