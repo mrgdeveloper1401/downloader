a36b0d5eaa5205a39258cf4580a6fad68bdf77d5
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-09-28 00:44:03 +0330

    clean project

--- card/admin.py
+++ card/admin.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
 from .models import  LinkSciol
 
-
-# @admin.register(CardModel)
-# class CardsAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'address', 'workspce', 'create_at', 'is_active', 'id')
-#     list_editable = ('is_active', )
-#     list_filter = ('is_active', 'create_at')
-
-
-# @admin.register(ResolotionSciolModel)
-# class CardHomeAdmin(admin.ModelAdmin):
-#     list_display = ('scial_name', 'format', 'create_at', 'id')
-#     list_filter = ('create_at',)
-    
 @admin.register(LinkSciol)
 class CardHomeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'link_sciol')
+    list_display = ('id', 'link_sciol', 'create_at')
--- card/models.py
+++ card/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 class LinkSciol(models.Model):
     link_sciol = models.URLField(_('link'), max_length=255)
+    create_at = models.DateTimeField(_('create link'), auto_now_add=True)
 
 
     class Meta:
